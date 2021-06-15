edb2f89f418a57ed1811a24dd2bfb53a9648ebd6
kendallha <kendallhaw@gmail.com>
2021-06-15 08:34:19 -0700

    Write post request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+    .then(response => response.json())
+}
+
+export const postUrl = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls')
+    .then(response => response.json())
 }
