19f0da8189532545c0b77b46ac032f2832e54c12
kendallha <kendallhaw@gmail.com>
2021-06-15 08:38:02 -0700

    Update post request to include body and headers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,14 @@ export const getUrls = () => {
     .then(response => response.json())
 }
 
-export const postUrl = (newUrl) => {
-  return fetch('http://localhost:3001/api/v1/urls')
+export const postUrl = ({ urltoShorten, title }) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify({
+      long_url: urltoShorten,
+      title: title
+    }),
+    headers: {'Content-type': 'application/json'}
+  })
     .then(response => response.json())
 }
