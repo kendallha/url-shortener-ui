c0f3ab3b67392033fdf504df3ef8e4b7e8ce4605
kendallha <kendallhaw@gmail.com>
2021-06-15 10:04:57 -0700

    Add test for form submission

--- cypress/integration/form_submission_spec.js
+++ cypress/integration/form_submission_spec.js
@@ -1,3 +1,44 @@
 describe('formSubmission', () => {
 
+  it('Should update the value of the form inputs when a user types in them', () => {
+    cy.intercept('http://localhost:3001/api/v1/urls',
+    {
+      "urls": [
+        {
+        "id": 11,
+        "long_url": "https://i.ytimg.com/vi/vYyUb_MI7to/maxresdefault.jpg",
+        "short_url": "http://localhost:3001/useshorturl/11",
+        "title": "Cool cat"
+        }
+      ]
+    }
+  )
+      .visit('http://localhost:3000')
+    cy.get('.title-input')
+      .type('Yummy')
+      .should('have.value', 'Yummy')
+  cy.get('.url-input')
+      .type('https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/')
+      .should('have.value', 'https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/')
+})
+  })
+
+  it('Should display a new shortened URL after the user submits the form', () => {
+    cy.intercept('POST', 'http://localhost:3001/sightings', {
+        statusCode: 201,
+        body: {
+          id: 12,
+          long_url: 'https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/',
+          short_url: 'http://localhost:3001/useshorturl/12',
+          description: 'Yummy'
+    },
+    })
+    cy.get('.title-input')
+      .type('Yummy')
+    cy.get('.url-input')
+      .type('https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/')
+    cy.get('button').click()
+    cy.get('.short-url').contains('http://localhost:3001/useshorturl/12')
+})
+
 })
